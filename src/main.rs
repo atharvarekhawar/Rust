a62d03ba7eca30446282fd1e08c4a5aa327c7b82
Atharva Rekhawar <atharvarekhawar64@gmail.com>
2023-12-26 15:01:17 +0530

    const , shadowing and parse method

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,29 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
-fn main() {
-    println!("What is your name?");
-    let mut name = String::new();
-    let greeting = "Nice to meet you";
+// fn main() {
+//     println!("What is your name?");
+//     let mut name = String::new();
+//     let greeting = "Nice to meet you";
 
-    io::stdin()
-        .read_line(&mut name)
-        .expect("Didn't receive input");
-    println!("Hello {}! {}", name.trim_end(), greeting);
+//     io::stdin()
+//         .read_line(&mut name)
+//         .expect("Didn't receive input");
+//     println!("Hello {}! {}", name.trim_end(), greeting);
+// }
+
+fn main() {
+    //const usually uses uppercase variable names
+    const ONE_MIL: u32 = 1_000_00; //unsigned integer 32 bits
+    const PI: f32 = 3.14; // float integer of 32 bits
+
+    //string have double quotes and char have single quotes
+    let age = "47";
+    let mut age: u32 = age.trim().parse().expect("Age was not assigned a number");
+
+    //we used same variable name for different data types anf that is called shadowing
+
+    age = age + 1;
+
+    println!("I am {} and I want {}", age, ONE_MIL);
 }
